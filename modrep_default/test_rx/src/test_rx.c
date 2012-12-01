96b40f1405fbc63a5f86f67a44bb046bbbd2d78c
ismagom <ismael.gomez@tsc.upc.edu>
2012-12-02 00:54:27 +0100

    cmake

--- modrep_default/test_rx/src/test_rx.c
+++ modrep_default/test_rx/src/test_rx.c
@@ -1,22 +1,103 @@
+/* 
+ * Copyright (c) 2012, Ismael Gomez-Miguelez <ismael.gomez@tsc.upc.edu>.
+ * This file is part of ALOE++ (http://flexnets.upc.edu/)
+ * 
+ * ALOE++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ALOE++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ALOE++.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
-#include <aloe/swapi.h>
+#include <aloe/swapi_static.h>
+#include <aloe/skeleton.h>
+#include <aloe/params.h>
 
-int cnt=0;
+#define MODULE_CONFIG
+#include "test_rx.h"
+#undef MODULE_CONFIG
 
-int Init(void *context) {
-	printf("rx is in init\n");
-	return 1;
-}
+int *block_length;
 
-int Run(void *context) {
-	cnt++;
-	printf("rx is in run\n");
+/*
+ * Function documentation
+ *
+ * @returns 0 on success, -1 on error
+ */
+int initialize() {
+
+	block_length = param_get_addr("block_length");
+	if (!block_length) {
+		moderror_msg("Error: %s\n",param_error_str());
+		return 0;
+	}
+
+	modinfo_msg("Parameter block_length is %d\n",*block_length);
+
+	/* Verify control parameters */
+	if (*block_length > get_input_max_samples()) {
+		moderror_msg("Invalid block length %d\n", *block_length);
+		return -1;
+	}
+
+
+	/* do some other initialization stuff */
+
 	return 0;
 }
 
-int Stop(void *context) {
-	printf("rx is stop\n");
-	swapi_exit(context);
+
+
+/**
+ * @brief Function documentation
+ *
+ * @param inp Input interface buffers. Data from other interfaces is stacked in the buffer.
+ * Use in(ptr,idx) to access the address. To obtain the number of received samples use the function
+ * int get_input_samples(int idx) where idx is the interface index.
+ *
+ * @param out Input interface buffers. Data to other interfaces must be stacked in the buffer.
+ * Use out(ptr,idx) to access the address.
+ *
+ * @return On success, returns a non-negative number indicating the output
+ * samples that should be transmitted through all output interface. To specify a different length
+ * for certain interface, use the function set_output_samples(int idx, int len)
+ * On error returns -1.
+ *
+ *
+ */
+int work(void **inp, void **out) {
+	int rcv_samples = get_input_samples(0); /** number of samples at itf 0 buffer */
+	int i;
+	input_t *input = inp[0];
+	output_t *output = out[0];
+
+
+	/* do DSP stuff here */
+	for (i=0;i<rcv_samples;i++) {
+		printf("input[%d/%d]=%.2f+i*%.2f\n",i, rcv_samples,__real__ input[i],__imag__ input[i]);
+	}
+	return rcv_samples;
+}
+
+/** @brief Deallocates resources created during initialize().
+ * @return 0 on success -1 on error
+ */
+int stop() {
 	return 0;
 }
+
+param_t *param_list(int *nof_params) {
+	*nof_params = nof_parameters;
+	return parameters;
+}
+
+
--- modrep_default/test_tx/src/test_tx.c
+++ modrep_default/test_tx/src/test_tx.c
@@ -1,34 +1,105 @@
+/* 
+ * Copyright (c) 2012, Ismael Gomez-Miguelez <ismael.gomez@tsc.upc.edu>.
+ * This file is part of ALOE++ (http://flexnets.upc.edu/)
+ * 
+ * ALOE++ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ALOE++ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ALOE++.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
-#include <aloe/swapi.h>
+#include <aloe/swapi_static.h>
+#include <aloe/skeleton.h>
+#include <aloe/params.h>
 
-int cnt;
+#define MODULE_CONFIG
+#include "test_tx.h"
+#undef MODULE_CONFIG
 
-itf_t output;
-int Init(void *context) {
-	printf("tx is in init\n");
-	output = swapi_itf_create(context, 0, ITF_WRITE, 229376);
-	if (!output) {
-		swapi_error_print(context,"itf_create");
+int *block_length;
+int cnt=0;
+
+/*
+ * Function documentation
+ *
+ * @returns 0 on success, -1 on error
+ */
+int initialize() {
+
+	block_length = param_get_addr("block_length");
+	if (!block_length) {
+		moderror_msg("Error: %s\n",param_error_str());
+		return 0;
+	}
+
+	modinfo_msg("Parameter block_length is %d\n",*block_length);
+
+	/* Verify control parameters */
+	if (*block_length > get_output_max_samples()) {
+		moderror_msg("Invalid block length %d\n", *block_length);
 		return -1;
 	}
-	return 1;
+
+
+	/* do some other initialization stuff */
+
+	return 0;
 }
 
-int x[20];
-int Run(void *context) {
-	cnt++;
-	int i,n;
-	for (i=0;i<20;i++) {
-		x[i]=cnt+i;
+
+
+/**
+ * @brief Function documentation
+ *
+ * @param inp Input interface buffers. Data from other interfaces is stacked in the buffer.
+ * Use in(ptr,idx) to access the address. To obtain the number of received samples use the function
+ * int get_input_samples(int idx) where idx is the interface index.
+ *
+ * @param out Input interface buffers. Data to other interfaces must be stacked in the buffer.
+ * Use out(ptr,idx) to access the address.
+ *
+ * @return On success, returns a non-negative number indicating the output
+ * samples that should be transmitted through all output interface. To specify a different length
+ * for certain interface, use the function set_output_samples(int idx, int len)
+ * On error returns -1.
+ *
+ *
+ */
+int work(void **inp, void **out) {
+	int snd_samples = *block_length;
+	int i;
+	input_t *input = inp[0];
+	output_t *output = out[0];
+
+	/* do DSP stuff here */
+	for (i=0;i<snd_samples;i++) {
+		__real__ input[i]=i+cnt;
+		__imag__ input[i]=snd_samples-i+cnt;
 	}
-	n=swapi_itf_write(output,x,20*sizeof(int));
-	printf("tx sends %d, cnt=%d\n",n,cnt);
+	cnt++;
+	return snd_samples;
+}
+
+/** @brief Deallocates resources created during initialize().
+ * @return 0 on success -1 on error
+ */
+int stop() {
 	return 0;
 }
 
-int Stop(void *context) {
-	printf("tx is stop\n");
-	swapi_exit(context);
-	return 0;
+param_t *param_list(int *nof_params) {
+	*nof_params = nof_parameters;
+	return parameters;
 }
+
+
